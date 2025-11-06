5a64d0f1f9a9a0837c2512d9578e9271824442c5
alkislaya <41301020+AlKislaya@users.noreply.github.com>
2025-11-06 23:31:20 +0300

    Implement filling up tableView with quotes logic

---
+++ InspoQuotes/Models/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  InspoQuotes
+//
+//  Created by Alexandra on 6.11.25.
+//
+
+struct Constants {
+    struct Outlets {
+        struct TableView {
+            struct ReusableCell {
+                static let quoteCell = "QuoteCell"
+            }
+        }
+    }
+}
